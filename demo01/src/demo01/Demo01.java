00d31306e4fe5ef368d479de291098b6c650b6e2
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-01-30 22:33:42 +0530

    create a class

--- demo01/src/demo01/Demo01.java
+++ demo01/src/demo01/Demo01.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package demo01;
 
-/**
- *
- * @author Ravindi Kumarasinghe
- */
+class Car{
+    
+}
+
 public class Demo01 {
 
     /**
