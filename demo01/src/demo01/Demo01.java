66c5dcff3e710f47c6da8c4ad9dbcae5f357941c
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-02-02 23:08:24 +0530

    while loop

--- demo01/src/demo01/Demo01.java
+++ demo01/src/demo01/Demo01.java
@@ -29,7 +29,11 @@ class Number{
     int i = 1;
     //declare & initialize variables
     
-   
+    while( i < 10) //loop as long as i<10
+    {
+    System.out.println("i"); //print values for i
+    i++;
+    }
     }
 }
 
